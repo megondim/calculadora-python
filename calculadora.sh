f7f8b879934f49e5311088dbbe63e24c880e0c66
Maria Eduarda Gondim <gondimme@gmail.com>
2025-08-19 00:27:12 -0300

    execução da calculadora
    arquivo shell

---
+++ calculadora.sh
@@ -0,0 +1,5 @@
+#!bin/bash                                                                                                                                          
+                                                                                                                                                    
+sudo apt update                                                                                                                                     
+sudo apt install python3                                                                                                                            
+python3 /home/dudagondim/modulo1/linux/calculadora_script.py 
