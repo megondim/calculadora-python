56233ad20c43429facc299fecdc9dfc3965081a9
Maria Eduarda Gondim <gondimme@gmail.com>
2025-08-19 00:25:51 -0300

    script
    inserir script da calculadora

---
+++ calculadora_script.py
@@ -0,0 +1,13 @@
+num1 = float(input("digite um número: "))                                                                                                           
+num2 = float(input("digite outro número: "))                                                                                                        
+escolha = input("escolha a operação (+, -, *, /): ")                                                                                                
+                                                                                                                                                    
+if escolha == "+":                                                                                                                                  
+        print("resultado: ", num1 + num2)                                                                                                           
+elif escolha == "-":                                                                                                                                
+        print("resultado: ", num1 - num2)                                                                                                           
+elif escolha == "*":                                                                                                                                
+        print("resultado: ", num1 * num2)                                                                                                           
+else:                                                                                                                                               
+        print("resultado: ", num1 / num2)                                                                                                           
+                                           
