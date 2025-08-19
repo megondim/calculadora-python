bdf6b75eb44fc2827de52f2f33b60c26f96cd036
Maria Eduarda Gondim <gondimme@gmail.com>
2025-08-19 00:38:12 -0300

    Update calculadora.sh
    atualização de caminho do local

--- calculadora.sh
+++ calculadora.sh
@@ -2,4 +2,4 @@
                                                                                                                                                     
 sudo apt update                                                                                                                                     
 sudo apt install python3                                                                                                                            
-python3 /home/dudagondim/modulo1/linux/calculadora_script.py 
+python3 calculadora_script.py 
